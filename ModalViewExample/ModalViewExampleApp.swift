3184c0d43b7004dd64f12057cfe2e84a0c5efefe
Natali <ns@numbuster.com>
2024-10-12 21:00:29 +0300

    Initial Commit

---
+++ ModalViewExample/ModalViewExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ModalViewExampleApp.swift
+//  ModalViewExample
+//
+//  Created by Natalia Sinitsyna on 12.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct ModalViewExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
